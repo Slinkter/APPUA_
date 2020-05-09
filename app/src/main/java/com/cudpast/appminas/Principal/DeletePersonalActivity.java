9e183cccfbacbb7bde9d69be0d4cb8de427d410c
Slinkter <luis.j.cueva@gmail.com>
2020-05-09 13:18:06 -0500

    demo : version 06

--- app/src/main/java/com/cudpast/appminas/Principal/DeletePersonalActivity.java
+++ app/src/main/java/com/cudpast/appminas/Principal/DeletePersonalActivity.java
@@ -4,17 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cudpast.appminas.Common.Common;
 import com.cudpast.appminas.Model.Personal;
 import com.cudpast.appminas.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DataSnapshot;
@@ -33,7 +40,7 @@ public class DeletePersonalActivity extends AppCompatActivity {
     private Button btndeletepersonal, btndeleteback, btndeletepersonalcosultardni;
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private Personal personal;
-
+    private LinearLayout layout_delete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +56,8 @@ public class DeletePersonalActivity extends AppCompatActivity {
         show_delete_personal = findViewById(R.id.show_delete_personal);
         btndeletepersonalcosultardni = findViewById(R.id.btndeletepersonalcosultardni);
 
+
+        layout_delete = findViewById(R.id.layout_delete);
         btndeletepersonal = findViewById(R.id.btndeletepersonal);
         btndeleteback = findViewById(R.id.btndeleteback);
 
@@ -66,10 +75,12 @@ public class DeletePersonalActivity extends AppCompatActivity {
         btndeletepersonal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(DeletePersonalActivity.this, "Eliminar ", Toast.LENGTH_SHORT).show();
+                String dni = delete_personal_dni.getText().toString();
+                showDiaglo(dni);
+
             }
         });
-        
+
 
         btndeleteback.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -99,11 +110,14 @@ public class DeletePersonalActivity extends AppCompatActivity {
                     show_delete_personal.setText(personal.getName());
                     show_delete_personal.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.color_error_null));
                     delete_personal_dni_layout.setError(null);
+                    layout_delete.setVisibility(View.VISIBLE);
+
                 } else {
                     Log.e(TAG, "el trabjador no existe en  ");
                     show_delete_personal.setText("");
                     show_delete_personal.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.color_error));
                     delete_personal_dni_layout.setError("El trabajador no exsite en la base de datos");
+                    layout_delete.setVisibility(View.INVISIBLE);
                 }
 
             }
@@ -135,6 +149,78 @@ public class DeletePersonalActivity extends AppCompatActivity {
         }
 
         return true;
+    }
+
+
+    public void showDiaglo(final String dni) {
+
+        android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(DeletePersonalActivity.this);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.pop_up_delete_personal, null);
+        builder.setView(view);
+        builder.setCancelable(false);
+        view.setKeepScreenOn(true);
+        final AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        //
+        Button btn_delete_yes = view.findViewById(R.id.btn_delete_yes);
+        Button btn_delete_no = view.findViewById(R.id.btn_delete_no);
+
+        btn_delete_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatabaseReference ref_db_mina_personal = database.getReference(Common.db_mina_personal);
+                DatabaseReference ref_mina = ref_db_mina_personal.child(Common.unidadTrabajoSelected.getNameUT());
+
+                DatabaseReference ref_db_mina_personal_data = database.getReference(Common.db_mina_personal_data);
+                final DatabaseReference ref_mina_data = ref_db_mina_personal_data.child(Common.unidadTrabajoSelected.getNameUT());
+
+                ref_mina.child(dni).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+
+                        ref_mina_data.child(dni).removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(DeletePersonalActivity.this, "Trabajador Eliminado ", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(DeletePersonalActivity.this, "Trabajador no  Eliminado", Toast.LENGTH_SHORT).show();
+                                dialog.dismiss();
+                            }
+                        });
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(DeletePersonalActivity.this, "Trabjador no  Eliminado", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                    }
+                });
+
+
+                Log.e(TAG, "ref_db_mina_personal" + ref_db_mina_personal);
+                Log.e(TAG, "ref_mina" + ref_mina);
+                Log.e(TAG, "ref_mina.child(dni)." + ref_mina.child(dni));
+
+            }
+        });
+
+
+        btn_delete_no
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                    }
+                });
+        dialog.show();
     }
 
 
