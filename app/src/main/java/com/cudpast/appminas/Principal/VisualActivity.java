daf54bfb25e4361d960f2a4b2ef3e24e5684ac7c
Slinkter <luis.j.cueva@gmail.com>
2020-05-13 16:12:39 -0500

    funciona el layout ScrollView , antes no funcionaba

--- app/src/main/java/com/cudpast/appminas/Principal/VisualActivity.java
+++ app/src/main/java/com/cudpast/appminas/Principal/VisualActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.cudpast.appminas.R;
 
@@ -21,7 +22,11 @@ import lecho.lib.hellocharts.view.LineChartView;
 public class VisualActivity extends AppCompatActivity {
 
 
-    LineChartView lineChartView;
+    LineChartView lineChartViewTemperatura;
+    LineChartView lineChartViewSaturacion;
+    LineChartView lineChartViewOxigeno;
+
+
     String[] axisData = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
     int[] yAxisData = {50, 20, 15, 30, 20, 60, 15, 40, 45, 10, 90, 18};
 
@@ -31,8 +36,26 @@ public class VisualActivity extends AppCompatActivity {
         setContentView(R.layout.activity_visual);
 
 
-        lineChartView = findViewById(R.id.chart1);
+        lineChartViewTemperatura = findViewById(R.id.chart1);
+        lineChartViewSaturacion = findViewById(R.id.chart2);
+        lineChartViewOxigeno = findViewById(R.id.chart3);
 
+
+
+
+
+
+        graficotem();
+        graficosatu();
+        graficoOxige();
+
+
+
+
+    }
+
+
+    private void graficotem() {
         List yAxisValues = new ArrayList();
         List axisValues = new ArrayList();
         Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0"));
@@ -58,17 +81,94 @@ public class VisualActivity extends AppCompatActivity {
         data.setAxisXBottom(axis);
 
         Axis yAxis = new Axis();
-      //  yAxis.setName("Temperatura");
+        yAxis.setName("Temperatura");
         yAxis.setTextColor(Color.parseColor("#03A9F4"));
         yAxis.setTextSize(16);
         data.setAxisYLeft(yAxis);
 
-        lineChartView.setLineChartData(data);
-        Viewport viewport = new Viewport(lineChartView.getMaximumViewport());
+        lineChartViewTemperatura.setLineChartData(data);
+        Viewport viewport = new Viewport(lineChartViewTemperatura.getMaximumViewport());
         viewport.top = 110;
-        lineChartView.setMaximumViewport(viewport);
-        lineChartView.setCurrentViewport(viewport);
-
+        lineChartViewTemperatura.setMaximumViewport(viewport);
+        lineChartViewTemperatura.setCurrentViewport(viewport);
 
     }
+
+    private void graficosatu() {
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0"));
+
+        for (int i = 0; i < axisData.length; i++) {
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++) {
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        axis.setTextSize(16);
+        axis.setTextColor(Color.parseColor("#03A9F4"));
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        yAxis.setName("Saturacion");
+        yAxis.setTextColor(Color.parseColor("#03A9F4"));
+        yAxis.setTextSize(16);
+        data.setAxisYLeft(yAxis);
+
+        lineChartViewSaturacion.setLineChartData(data);
+        Viewport viewport = new Viewport(lineChartViewSaturacion.getMaximumViewport());
+        viewport.top = 110;
+        lineChartViewSaturacion.setMaximumViewport(viewport);
+        lineChartViewSaturacion.setCurrentViewport(viewport);
+    }
+
+    private void graficoOxige() {
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0"));
+
+        for (int i = 0; i < axisData.length; i++) {
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++) {
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        axis.setTextSize(16);
+        axis.setTextColor(Color.parseColor("#03A9F4"));
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        yAxis.setName("Oxigeno");
+        yAxis.setTextColor(Color.parseColor("#03A9F4"));
+        yAxis.setTextSize(16);
+        data.setAxisYLeft(yAxis);
+
+        lineChartViewOxigeno.setLineChartData(data);
+        Viewport viewport = new Viewport(lineChartViewOxigeno.getMaximumViewport());
+        viewport.top = 110;
+        lineChartViewOxigeno.setMaximumViewport(viewport);
+        lineChartViewOxigeno.setCurrentViewport(viewport);
+    }
+
+
 }
