7d5dd718a61cb729ef83f346956a1b5efcd21aad
Slinkter <luis.j.cueva@gmail.com>
2020-08-03 12:11:15 -0500

    exito, se settea los datos del personal en Layout , faltaria el codigo de actualizar

--- app/src/main/java/com/cudpast/appminas/Principal/Activities/EditWorkerActivity.java
+++ app/src/main/java/com/cudpast/appminas/Principal/Activities/EditWorkerActivity.java
@@ -34,14 +34,19 @@ import com.google.firebase.database.ValueEventListener;
 public class EditWorkerActivity extends AppCompatActivity {
 
     public static final String TAG = EditWorkerActivity.class.getSimpleName();
-    private TextInputLayout delete_personal_dni_layout;
-    private TextInputEditText delete_personal_dni;
+    private TextInputLayout query_personal_dni_layout;
+    private TextInputEditText query_personal_dni;
     private TextView show_delete_personal;
 
-    private Button btnDeletepersonal, btnDeleteback, btnCosultardni;
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    TextInputLayout update_personal_name_layout, update_personal_last_layout, update_personal_address_layout, update_personal_phone1_layout;
+    TextInputEditText update_personal_name, update_personal_last, update_personal_address, update_personal_phone1;
+
+
+    private Button btn_update_personal, btn_exit_back, btn_query_dni_personal;
+    private FirebaseDatabase database;
     private Personal personal;
-    private LinearLayout layout_delete;
+    private LinearLayout layout_btn_update;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,49 +55,51 @@ public class EditWorkerActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.activity_delete_worker);
 
+        database = FirebaseDatabase.getInstance();
 
-        delete_personal_dni_layout = findViewById(R.id.delete_personal_dni_layout);
-        delete_personal_dni = findViewById(R.id.delete_personal_dni);
+        query_personal_dni_layout = findViewById(R.id.query_personal_dni_layout);
+        query_personal_dni = findViewById(R.id.query_personal_dni);
+        btn_query_dni_personal = findViewById(R.id.btn_query_dni_personal);
+
+        update_personal_name_layout = findViewById(R.id.update_personal_name_layout);
+        update_personal_last_layout = findViewById(R.id.update_personal_last_layout);
+        update_personal_address_layout = findViewById(R.id.update_personal_address_layout);
+        update_personal_phone1_layout = findViewById(R.id.update_personal_phone1_layout);
+
+
+        update_personal_name = findViewById(R.id.update_personal_name);
+        update_personal_last = findViewById(R.id.update_personal_last);
+        update_personal_address = findViewById(R.id.update_personal_address);
+        update_personal_phone1 = findViewById(R.id.update_personal_phone1);
+
+
+        layout_btn_update = findViewById(R.id.layout_btn_update);
+        btn_update_personal = findViewById(R.id.btn_update_personal);
+        btn_exit_back = findViewById(R.id.btn_exit_back);
 
         show_delete_personal = findViewById(R.id.show_delete_personal);
-        btnCosultardni = findViewById(R.id.btndeletepersonalcosultardni);
 
 
-        layout_delete = findViewById(R.id.layout_delete);
-        btnDeletepersonal = findViewById(R.id.btndeletepersonal);
-        btnDeleteback = findViewById(R.id.btndeleteback);
-
-
-        btnCosultardni.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (submitForm()) {
-                    String dni = delete_personal_dni.getText().toString();
-                    consultarDniPersonal(dni);
-                }
+        // Realiza la consulta del dni si exsite el trabajador
+        btn_query_dni_personal.setOnClickListener(v -> {
+            if (submitForm()) {
+                String dni = query_personal_dni.getText().toString();
+                consultarDniPersonal(dni);
             }
         });
 
+        // Realiza la actualizacion del trabajador
+        btn_update_personal.setOnClickListener(v -> {
+            String dni = query_personal_dni.getText().toString();
+            showDiaglo(dni);
 
-
-
-        btnDeletepersonal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String dni = delete_personal_dni.getText().toString();
-                showDiaglo(dni);
-
-            }
         });
 
-
-        btnDeleteback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(EditWorkerActivity.this, AllActivity.class);
-                startActivity(intent);
-                finish();
-            }
+        // Salir
+        btn_exit_back.setOnClickListener(v -> {
+            Intent intent = new Intent(EditWorkerActivity.this, AllActivity.class);
+            startActivity(intent);
+            finish();
         });
 
 
@@ -100,47 +107,69 @@ public class EditWorkerActivity extends AppCompatActivity {
 
 
     private void consultarDniPersonal(String dni_personal) {
-        DatabaseReference ref_db_mina_personal = database.getReference(Common.db_mina_personal);
-        DatabaseReference ref_mina = ref_db_mina_personal.child(Common.unidadTrabajoSelected.getNameUT());
-        ref_mina.child(dni_personal).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                personal = dataSnapshot.getValue(Personal.class);
-                if (personal != null) {
-                    Log.e(TAG, "nombre : " + personal.getName());
-                    Log.e(TAG, "dni : " + personal.getDni());
-                    Log.e(TAG, "dirección : " + personal.getAddress());
-                    Log.e(TAG, "phone 1 : " + personal.getPhone1());
-                    show_delete_personal.setText(personal.getName() + " " + personal.getLast());
-                    show_delete_personal.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.color_error_null));
-                    delete_personal_dni_layout.setError(null);
-                    layout_delete.setVisibility(View.VISIBLE);
 
-                } else {
-                    Log.e(TAG, "el trabjador no existe en  ");
-                    show_delete_personal.setText("");
-                    show_delete_personal.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.color_error));
-                    delete_personal_dni_layout.setError("El trabajador no exsite en la base de datos");
-                    layout_delete.setVisibility(View.INVISIBLE);
-                }
+        DatabaseReference ref_db_mina_personal = database
+                .getReference(Common.db_mina_personal)
+                .child(Common.unidadTrabajoSelected.getNameUT())
+                .child(dni_personal);
 
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e(TAG, "error : " + databaseError.getMessage());
-            }
-        });
+        ref_db_mina_personal
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        personal = dataSnapshot.getValue(Personal.class);
+                        if (personal != null) {
+                            Log.e(TAG, "nombre : " + personal.getName());
+                            Log.e(TAG, "dni : " + personal.getDni());
+                            Log.e(TAG, "dirección : " + personal.getAddress());
+                            Log.e(TAG, "phone 1 : " + personal.getPhone1());
+                            show_delete_personal.setText(personal.getName() + " " + personal.getLast());
+                            show_delete_personal.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.color_error_null));
+                            query_personal_dni_layout.setError(null);
+                            layout_btn_update.setVisibility(View.VISIBLE);
+
+                            //
+                            update_personal_name.setText(personal.getName());
+                            update_personal_last.setText(personal.getLast());
+                            update_personal_address.setText(personal.getAddress());
+                            update_personal_phone1.setText(personal.getPhone1());
+                            //
+
+
+
+                        } else {
+                            Log.e(TAG, "el trabjador no existe en  ");
+                            show_delete_personal.setText("");
+                            show_delete_personal.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.color_error));
+                            query_personal_dni_layout.setError("El trabajador no exsite en la base de datos");
+                            query_personal_dni_layout.requestFocus();
+                            layout_btn_update.setVisibility(View.INVISIBLE);
+                            //
+                            update_personal_name.setText(" ");
+                            update_personal_last.setText(" ");
+                            update_personal_address.setText(" ");
+                            update_personal_phone1.setText(" ");
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        Log.e(TAG, "error : " + databaseError.getMessage());
+                    }
+                });
 
     }
 
 
     private boolean checkDNI() {
-        if (delete_personal_dni.getText().toString().trim().isEmpty()) {
-            delete_personal_dni_layout.setError("Ingrese su DNI");
+        if (query_personal_dni.getText().toString().trim().isEmpty()) {
+            query_personal_dni_layout.setError("Ingrese su DNI");
+            query_personal_dni_layout.requestFocus();
             return false;
         } else {
-            delete_personal_dni_layout.setError(null);
+            query_personal_dni_layout.setError(null);
         }
         return true;
     }
